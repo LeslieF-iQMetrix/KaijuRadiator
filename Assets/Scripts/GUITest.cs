6f6b5d7fe1037a90f7d06eb3610896f42def1982
Leslie Fung <leslief@iqmetrix.com>
2014-07-19 19:37:32 -0700

    Initial commit, can connect to TeamCity

---
+++ Assets/Scripts/GUITest.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+public class GUITest : MonoBehaviour
+{
+
+		private static readonly string TeamcityUrl = "https://teamcity.iqmetrix.com/httpAuth/app/rest/";
+		private string username = "Username";
+		private string password = "Password";
+
+		void OnGUI ()
+		{
+				GUI.depth = 20;
+				GUIStyle bigFontStyleTextField = new GUIStyle (GUI.skin.textField);
+				GUIStyle bigFontStyleBox = new GUIStyle (GUI.skin.box);
+				bigFontStyleTextField.fontSize = 24;
+				bigFontStyleBox.fontSize = 30;
+				// Make a background box
+				GUI.Box (new Rect ((Screen.width / 2) - 240, (Screen.height / 2) - 120, 480, 240), "TeamCity Login", bigFontStyleBox);
+
+				GUI.SetNextControlName ("Username");
+				username = GUI.TextField (new Rect ((Screen.width / 2) - 220, (Screen.height / 2) - 60, 400, 40), username, bigFontStyleTextField);
+				GUI.SetNextControlName ("Password");
+				password = GUI.PasswordField (new Rect ((Screen.width / 2) - 220, (Screen.height / 2), 400, 40), password, '*', bigFontStyleTextField);
+				if (GUI.Button (new Rect ((Screen.width / 2) + 100, (Screen.height / 2) + 60, 120, 40), "LOGIN")) {
+						StartCoroutine (Login());
+				}
+
+				// The textfields getting focus makes textfields blank if still at default value
+				if (Event.current.type == EventType.Repaint) {
+						if (GUI.GetNameOfFocusedControl () == "Username") {
+								if (username.Equals ("Username"))
+										username = "";
+						} else {
+								if (username.Equals (""))
+										username = "Username";
+						}
+
+						if (GUI.GetNameOfFocusedControl () == "Password") {
+								if (password.Equals ("Password"))
+										password = "";
+						} else {
+								if (password.Equals (""))
+										password = "Password";
+						}
+				}
+		}
+
+		IEnumerator Login ()
+		{
+				var login = String.Format ("{0}:{1}", username, password);
+				var headers = new Dictionary<string, string> ();
+				headers ["Authorization"] = "Basic " + System.Convert.ToBase64String (System.Text.Encoding.ASCII.GetBytes (login)); 
+				var www = new WWW (TeamcityUrl, null, headers);
+				yield return www;
+				if (www.error != null) {
+						Debug.Log (www.error);
+						yield break;
+				}
+				
+				Debug.Log(string.Format ("Successful login by user {0}", username));
+				GameObject.Find ("LoginGUI").AddComponent ("LoginMessage");
+	}
+}
---
+++ Assets/Scripts/LoginMessage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginMessage : MonoBehaviour
+{
+		void OnGUI ()
+		{
+		GUI.depth = 10;
+				GUIStyle successStyle = new GUIStyle (GUI.skin.label);
+				successStyle.normal.textColor = Color.green;
+				successStyle.fontSize = 50;
+				successStyle.fontStyle = FontStyle.Bold;
+				successStyle.normal.background = MakeTex (200, 200, Color.black);
+				successStyle.alignment = TextAnchor.MiddleCenter;
+				GUI.Label (new Rect ((Screen.width / 2) - 200, (Screen.height / 2) - 100, 480, 240), "YOU LOGGED IN", successStyle);
+		}
+
+		private Texture2D MakeTex (int width, int height, Color col)
+		{
+				Color[] pix = new Color[width * height];
+		
+				for (int i = 0; i < pix.Length; i++)
+						pix [i] = col;
+		
+				Texture2D result = new Texture2D (width, height);
+				result.SetPixels (pix);
+				result.Apply ();
+		
+				return result;
+		}
+}
